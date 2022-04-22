4e9663fd375d73b3dbc5457a31ed31adb47fa8d4
est8r <estermessias6@gmail.com>
2022-04-22 18:28:28 -0300

    Modifiquei o fundo da página inical, o logo e o JS

--- personagem3/dustin2/index.js
+++ personagem3/dustin2/index.js
@@ -1,17 +1,17 @@
 function validaResposta() {
   var certo = 1748;
   var tentativas = 0;
+  var resposta = parseInt(prompt("Sua Resposta"));
 
-  while (tentativas < 3) {
-    var resposta = parseInt(prompt("Sua Resposta"));
-    if (certo == resposta) {
-      alert("Voce acertou! Clique em OK para ir para a próxima fase");
-      window.open("../dustin3/index.html");
-      window.close();
-    } else {
+  if (certo == resposta) {
+    alert("Voce acertou! Clique em OK para ir para a próxima fase");
+    window.location.assign("../dustin3/index.html");
+  } else {
+    while (tentativas < 3) {
       alert("Tente novamente.");
       tentativas++;
+      var resposta = parseInt(prompt("Sua Resposta"));
     }
+    window.location.replace("../../gameover/index.html");
   }
-  window.location.replace("../../gameover/index.html");
 }
